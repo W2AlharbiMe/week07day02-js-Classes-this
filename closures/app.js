96f6660ce7aaf7fd641e581a88d8c3e6ae7d34b3
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2019-12-29 15:46:36 +0300

    classes 102

--- closures/app.js
+++ closures/app.js
@@ -42,6 +42,47 @@ let add = (function() {
 
 
 
+// classes 101
+
+// const celica = {
+//     model: 'toyota',
+//     color: 'black',
+//     fuel: 100,
+//     drive() {
+//         this.fuel--;
+//         return 'Driving';
+//     },
+//     refuel() {
+//         this.fuel = 100;
+//         return 'fueled up' 
+//     }
+// };
+
+// console.log(celica.model)
+// console.log(celica.drive() + "  " + celica.fuel);
+// console.log(celica.refuel());
 
 
 
+// classes 102
+
+class Car {
+    constructor(model, color) {
+        this.model = model;
+        this.color = color;
+        this.fuel = 100;
+    }
+
+    drive() {
+        this.fuel--;
+        return `Current fuel is ${this.fuel}. while driving.`
+    }
+
+    refuel() {
+        this.fuel = 100;
+        return `Current fuel is ${this.fuel}. while refueling.`
+    }
+}
+
+let celica = new Car('Toyota', 'Black')
+console.log(celica)
